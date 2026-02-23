e9e143bc97b8ffb074b4db9da2454fd40774a824
AgustinUno <justinelloydgbautista@iskolarngbayan.pup.edu.ph>
2026-02-23 09:03:48 +0800

    fe-version refactor
    full version refactor of tailwind and shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
-// src/App.tsx
-import { BrowserRouter } from "react-router-dom";
-import AppRoutes from "./routes/AppRoutes";
+import { Routes, Route, Navigate } from "react-router-dom"
+import LoginPage from "./pages/LoginPage"
+import DashboardPage from "./pages/DashboardPage"
 
-function App() {
+export default function App() {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
-  );
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
+    </Routes>
+  )
 }
-
-export default App;
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,21 +1,9 @@
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5029',
-        rewrite: path => path.replace(/^\/api/, ''),
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
