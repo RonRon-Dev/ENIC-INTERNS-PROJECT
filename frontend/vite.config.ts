025610a91b59aac5429583ae97eb3a6d2497cca9
1101101011 <ilardecharles@gmail.com>
2026-03-02 16:43:45 +0800

    MISC: Updated Dockerfile config and stuff

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,24 +1,28 @@
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 import react from "@vitejs/plugin-react"
 import path from "path"
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5029',
-        rewrite: path => path.replace(/^\/api/, ''),
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    server: {
+      host: '0.0.0.0',
+      port: 5173,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          // rewrite: (path) => path.replace(/^\/api/, ''),
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+  }
 })
