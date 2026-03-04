40659dbb66b6371997635bfbe453ca4020b61d4e
mark <fulguerinasmarkjason0311@gmail.com>
2026-03-04 11:22:09 +0800

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -10,7 +10,7 @@ import { Toaster } from "./components/ui/sonner"
 createRoot(document.getElementById("root")!).render(
 
   <StrictMode>
-    <Toaster duration={5000} />
+    <Toaster duration={5000} position="top-right"/>
     <BrowserRouter>
       <App />
     </BrowserRouter>
