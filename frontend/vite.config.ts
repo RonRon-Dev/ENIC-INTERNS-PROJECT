e779d3639ad717326d333f42d7e75b26813b095b
AgustinUno <justinelloydgbautista@iskolarngbayan.pup.edu.ph>
2026-03-05 08:30:41 +0800

    ADDED: VITE BUILD
    TEST FOR DEPLOYMENT BUILD CONFIG

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,18 @@
-import { defineConfig, loadEnv } from "vite"
-import react from "@vitejs/plugin-react"
-import path from "path"
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, process.cwd(), "");
 
   return {
     plugins: [react()],
     server: {
-      host: '0.0.0.0',
+      host: "0.0.0.0",
       port: 5173,
       proxy: {
-        '/api': {
+        "/api": {
           target: env.VITE_API_URL,
-          // rewrite: (path) => path.replace(/^\/api/, ''),
           changeOrigin: true,
           secure: false,
         },
@@ -24,5 +23,9 @@ export default defineConfig(({ mode }) => {
         "@": path.resolve(__dirname, "./src"),
       },
     },
-  }
-})
+    build: {
+      outDir: "build",
+      emptyOutDir: true,
+    },
+  };
+});
